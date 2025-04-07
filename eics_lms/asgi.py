77892baa3aaff718888fe7ca230ae1eec70e3d3f
Amimo2 <87418031+Amimo2@users.noreply.github.com>
2025-04-07 11:30:54 +0000

    asgi.py

---
+++ eics_lms/asgi.py
@@ -0,0 +1,6 @@
+# eics_lms/asgi.py
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eics_lms.settings')
+application = get_asgi_application()
