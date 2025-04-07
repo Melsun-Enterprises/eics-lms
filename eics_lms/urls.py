d0265bb34c30a7f2dad66788d9ba0364a77e6896
Amimo2 <87418031+Amimo2@users.noreply.github.com>
2025-04-07 11:34:02 +0000

    urls.py

---
+++ eics_lms/urls.py
@@ -0,0 +1,6 @@
+# eics_lms/wsgi.py
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eics_lms.settings')
+application = get_wsgi_application()
